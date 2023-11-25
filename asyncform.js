84c21a513d427e96a2a5adbef57b61c27f4966c4
John Shedletsky <113091876+ui0ppk@users.noreply.github.com>
2023-11-25 23:57:28 +0000

    Update asyncform.js

--- asyncform.js
+++ asyncform.js
@@ -4,13 +4,14 @@
   / _` / __| | | | '_ \ / __| |_ / _ \| '__| '_ ` _ \   | / __|
  | (_| \__ \ |_| | | | | (__|  _| (_) | |  | | | | | |_ | \__ \
   \__,_|___/\__, |_| |_|\___|_|  \___/|_|  |_| |_| |_(_)/ |___/
-  qzip      |___/                                     |__/      v1.0.1
+  qzip      |___/                                     |__/      v1.0.2
 _______________________________________________________________________
     a simple way of doing forms without refreshing your page!
         this works in the literal same way a form does.
 */
 document.addEventListener('submit', async function (event) {
   const form = event.target;
+  const button = event.submitter;
     if(form.getAttribute('type').toLowerCase() == "asyncform") {
       event.preventDefault();
       event.stopPropagation();
@@ -19,13 +20,19 @@ document.addEventListener('submit', async function (event) {
     }
 
     const method = (form.getAttribute('method')).toUpperCase() || 'GET';
-    const action = form.getAttribute('action');
-    const headers =  JSON.parse(form.getAttribute('headers') || "{}") || {};
+    const action = form.getAttribute('action') || false;
+    const btnaction = button.getAttribute('action') || false;
+    const headers = JSON.parse(form.getAttribute('headers') || "{}") || {};
     const responsehtml = form.getAttribute('responsehtml') || null;
     const responsejs = form.getAttribute('responsejs') || null;
 
     const formdata = new FormData(form);
-    const url = new URL(action, window.location.origin);
+    let url;
+    url = btnaction ? new URL(btnaction, window.location.origin) : new URL(action, window.location.origin);
+
+    if(action == null) {
+    return;
+    }
 
     let response;
 
@@ -34,7 +41,6 @@ document.addEventListener('submit', async function (event) {
             url.searchParams.append(key, value);
         }
     }
-
     headers['Content-Type'] = 'application/x-www-form-urlencoded';
     try {
       response = await fetch(url, {
